8e14a42b73efd35623392161a4cfb1aae70f1d08
evsDen <saratov642000@gmail.com>
2022-11-12 16:42:30 +0400

    Окончание Домашней работы 9

---
+++ Homework9.cs
@@ -0,0 +1,49 @@
+// // Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+// // (НЕ ИСПОЛЬЗОВАТЬ РЕЗУЛЬТИРУЮЩУЮ ПЕРЕМЕННУЮ)
+// int resultSum(int m, int n)
+// {
+//     if (m == n)
+//        return n;
+//     return ((m+n)*(n-m+1)/2);
+// }
+
+
+// Console.Clear();
+// Console.Write("Введите 1-ое число: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите 2-ое число: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(resultSum(m, n));
+
+
+// // Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+// Console.Write("Введите число M: ");
+// int m = Convert.ToInt32(Console.ReadLine());
+
+// Console.Write("Введите число N: ");
+// int n = Convert.ToInt32(Console.ReadLine());
+
+// AkkermanFunction(m,n);
+
+// void AkkermanFunction(int m, int n)
+// {
+//     Console.Write(Akkerman(m, n)); 
+// }
+
+// int Akkerman(int m, int n)
+// {
+//     if (m == 0)
+//     {
+//         return n + 1;
+//     }
+//     else if (n == 0 && m > 0)
+//     {
+//         return Akkerman(m - 1, 1);
+//     }
+//     else
+//     {
+//         return (Akkerman(m - 1, Akkerman(m, n - 1)));
+//     }
+// }
+
+ 
